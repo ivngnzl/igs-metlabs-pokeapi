dd1cf4f62bdd25bf94b8b153796055378deac78c
ivngnzl <ivangonzalez012@gmail.com>
2025-07-31 18:03:26 +0200

    feat: Not found pokemon page

--- src/app/pokemon/[index]/page.tsx
+++ src/app/pokemon/[index]/page.tsx
@@ -2,6 +2,7 @@ import { getPokemon } from '@/lib/pokeapi'
 import PokemonDetail from '@/components/PokemonDetail'
 import { notFound } from 'next/navigation'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export async function generateStaticParams() {
   return Array.from({ length: 9 }, (_, i) => ({
@@ -20,10 +21,18 @@ export default async function PokemonPage({
   if (isNaN(id) || id < 1 || id > 9) {
     return (
       <>
-        <div className="p-6 text-center mx-96">
+        <div className="p-6 text-center  mt-6 text-balance">
           Solo aceptamos las 3 evoluciones de los 3 pokemons iniciales de la
-          primera generación, somos vieja escuela. ¿Algún problema? 😠
+          primera generación. Somos de la vieja escuela. ¿Algún problema? 😠
         </div>
+        <Image
+          src="/pikachu.webp"
+          alt="Pikachu"
+          width={200}
+          height={200}
+          className="mx-auto mt-4 rounded-2xl aspect-square object-cover shadow-lg"
+          priority
+        />
         <Link
           href={'/'}
           className="mt-4 block text-gray-100 underline text-center"
